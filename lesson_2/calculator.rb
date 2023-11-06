394c28ebee8f7c0147eb6e2045ed5beaeb360f78
MarilynRoxie <marilynroxie@gmail.com>
2023-11-06 10:40:38 -0800

    Applied Rubocop suggestions to calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -89,16 +89,15 @@ MSG
   prompt("#{operation_to_message(operator)} the two numbers...")
 
   result = case operator
-    when "1"
-      number1.to_i() + number2.to_i()
-    when "2"
-      number1.to_i() - number2.to_i()
-    when "3"
-      number1.to_i() * number2.to_i()
-    when "4"
-      number1.to_f() / number2.to_f()
-    else
-    end
+           when "1"
+             number1.to_i() + number2.to_i()
+           when "2"
+             number1.to_i() - number2.to_i()
+           when "3"
+             number1.to_i() * number2.to_i()
+           when "4"
+             number1.to_f() / number2.to_f()
+           end
 
   prompt("The result is #{result}")
 
