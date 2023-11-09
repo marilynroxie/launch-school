1bf85fe9cee321f68919f5edecfb4f1d5e75bc39
MarilynRoxie <marilynroxie@gmail.com>
2023-11-09 11:46:27 -0800

    Added default arguments solution

---
+++ R101-RB119-Small-Problems/1_documentation_again/3_default_arguments_in_the_middle.rb
@@ -0,0 +1,9 @@
+def my_method(a, b = 2, c = 3, d)
+  p [a, b, c, d]
+end
+
+my_method(4, 5, 6)
+
+# => 4, 5, 3, 6
+
+# https://ruby-doc.org/3.2.2/syntax/calling_methods_rdoc.html#label-Positional+Argumentsl
