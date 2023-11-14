711bc83bbd8f4e40808b551fc2698711a542dd8e
MarilynRoxie <marilynroxie@gmail.com>
2023-11-13 18:41:28 -0800

    Starting calculator with bonus features

--- lesson_2/calculator_with_bonus_features.rb
+++ lesson_2/calculator_with_bonus_features.rb
@@ -108,3 +108,25 @@ MSG
 end
 
 prompt("Thank you for using the calculator. Good bye!")
+
+=begin
+
+Calculator Bonus Features
+
+1. Better integer validation
+
+The current method of validating the input for a number is very weak. It's also not fully accurate, as you cannot enter a 0. Come up with a better way of validating input for integers.
+
+2. Number validation.
+
+Suppose we're building a scientific calculator, and we now need to account for inputs that include decimals. How can we build a validating method, called number?, to verify that only valid numbers -- integers or floats -- are entered?
+
+3. Our operation_to_message method is a little dangerous, since we're relying on the case statement being the last expression in the method. Suppose we needed to add some code after the case statement within the method? What changes would be needed to keep the method working with the rest of the program?
+
+4. Extracting messages in the program to a configuration file.
+
+There are lots of messages sprinkled throughout the program. Could we move them into some configuration file and access by key? This would allow us to manage the messages much easier, and we could even internationalize the messages.
+
+5. Your calculator program is a hit, and it's being used all over the world! Problem is, not everyone speaks English. You need to now internationalize the messages in your calculator. You've already done the hard work of extracting all the messages to a configuration file. Now, all you have to do is send that configuration file to translators and call the right translation in your code.
+
+=end
