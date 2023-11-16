91d96fee696c05b32662558bff37fead77f454bd
MarilynRoxie <marilynroxie@gmail.com>
2023-11-16 14:30:29 -0800

    Added solution for madlibs

---
+++ R101-RB119-Small-Problems/2_easy_1/7_madlibs.rb
@@ -0,0 +1,12 @@
+loop do
+  puts "Enter a noun: "
+  noun = gets.chomp
+  puts "Enter a verb: "
+  verb = gets.chomp
+  puts "Enter an adjective: "
+  adjective = gets.chomp
+  puts "Enter an adverb: "
+  adverb = gets.chomp
+  puts "You #{verb} your #{adjective} #{noun} #{adverb}?"
+  break
+end
