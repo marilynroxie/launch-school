dee21d3d0d8d5bab271e9ee0b5785feafc533a43
MarilynRoxie <marilynroxie@gmail.com>
2023-11-16 13:58:39 -0800

    Added bannerizer solution

---
+++ R101-RB119-Small-Problems/2_easy_1/5_bannerizer.rb
@@ -0,0 +1,31 @@
+def print_in_box(text)
+  lines = text.length
+  first = "+-"
+  last = "-+"
+  bar = "|"
+  hyphen = "-"
+
+  print first
+
+  lines.times do
+    print hyphen
+  end
+  print last
+  puts " "
+
+  puts bar + " " * lines + "  #{bar}"
+
+  puts "#{bar} #{text} #{bar} "
+
+  puts bar + " " * lines + "  #{bar}"
+
+  print first
+  lines.times do
+    print hyphen
+  end
+
+  puts last
+end
+
+print_in_box("To boldly go where no one has gone before.")
+print_in_box("")
