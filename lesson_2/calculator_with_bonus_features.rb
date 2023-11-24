376e14fd589e8026801f541e0d55f364857aaf4f
MarilynRoxie <marilynroxie@gmail.com>
2023-11-24 13:44:21 -0800

    Added clear screen before welcome message, started experimenting with rufo config

--- lesson_2/calculator_with_bonus_features.rb
+++ lesson_2/calculator_with_bonus_features.rb
@@ -1,9 +1,12 @@
-require "yaml"
-MESSAGES = YAML.load_file("messages.yml")
+require 'yaml'
+MESSAGES = YAML.load_file('messages.yml')
 
 # Todo
 # Save case statement in variable or hash?
 # Add translations
+# Finish rufo config for case/when
+
+system 'clear'
 
 def prompt(message)
   puts("=> #{message}")
@@ -16,109 +19,107 @@ def valid_number?(input)
 end
 
 def operation_to_message(operation)
-  choice = case operation
-    when "1"
-      "Adding"
-    when "2"
-      "Subtracting"
-    when "3"
-      "Multiplying"
-    when "4"
-      "Dividing"
-    end
-
-  choice
+  case operation
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
 end
 
-prompt(MESSAGES["welcome"])
+prompt(MESSAGES['welcome'])
 
-name = ""
+name = ''
 loop do
   name = gets.chomp.strip.capitalize
 
   if name.empty?
-    prompt(MESSAGES["valid_name"])
+    prompt(MESSAGES['valid_name'])
   else
     break
   end
 end
 
-prompt format(MESSAGES["hi"], name)
+prompt format(MESSAGES['hi'], name)
 
 # Ask the user for two numbers
 
 loop do # main loop
-  number1 = ""
+  number1 = ''
 
   loop do
-    prompt format(MESSAGES["what_number"], "first")
+    prompt format(MESSAGES['what_number'], 'first')
     number1 = gets.chomp
     if valid_number?(number1)
-      number1 = number1.include?(".") ? number1.to_f : number1.to_i
+      number1 = number1.include?('.') ? number1.to_f : number1.to_i
       break
     else
-      prompt(MESSAGES["valid_number"])
+      prompt(MESSAGES['valid_number'])
     end
   end
 
-  number2 = ""
+  number2 = ''
 
   loop do
-    prompt format(MESSAGES["what_number"], "second")
+    prompt format(MESSAGES['what_number'], 'second')
     number2 = gets.chomp
     if valid_number?(number2)
-      number2 = number2.include?(".") ? number2.to_f : number2.to_i
+      number2 = number2.include?('.') ? number2.to_f : number2.to_i
       break
     else
-      prompt(MESSAGES["valid_number"])
+      prompt(MESSAGES['valid_number'])
     end
   end
 
-  system "clear"
+  system 'clear'
 
   # Ask the user for an operation to perform
 
-  prompt format(MESSAGES["operator_prompt"], number1, number2)
+  prompt format(MESSAGES['operator_prompt'], number1, number2)
 
-  operator = ""
+  operator = ''
   loop do
     operator = gets.chomp
 
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt(MESSAGES["valid_choice"])
+      prompt(MESSAGES['valid_choice'])
     end
   end
 
-  prompt format(MESSAGES["calculating"], operation_to_message(operator))
+  prompt format(MESSAGES['calculating'], operation_to_message(operator))
 
   # Perform the operation on the two numbers
 
   result = case operator
-    when "1"
+    when '1'
       number1 + number2
-    when "2"
+    when '2'
       number1 - number2
-    when "3"
+    when '3'
       number1 * number2
-    when "4"
+    when '4'
       result = number1.to_f / number2.to_f
-      result % 1 == 0 ? result.to_i : result
+      result % 1 == 0 ? result.to_i : result.to_f
     end
 
   # Handle zero division and output the result
   if (number2.to_i.zero? && result.infinite?) || result.to_f.nan?
-    prompt(MESSAGES["zero_division"])
+    prompt(MESSAGES['zero_division'])
   else
-    prompt format(MESSAGES["result"], result)
+    prompt format(MESSAGES['result'], result)
   end
 
-  prompt(MESSAGES["another_calc"])
+  prompt(MESSAGES['another_calc'])
 
   answer = gets.chomp
-  break unless answer.downcase.start_with?("y")
-  system "clear"
+  break unless answer.downcase.start_with?('y')
+  system 'clear'
 end
 
-prompt format(MESSAGES["thank_you"], name)
+prompt format(MESSAGES['thank_you'], name)
