b4497ca0ce19a38ddfc6995f5179419bdc2f0438
MarilynRoxie <marilynroxie@gmail.com>
2023-12-05 18:23:39 -0800

    Separated code into methods

--- RB101/lesson_2/mortgage_car_loan_calculator.rb
+++ RB101/lesson_2/mortgage_car_loan_calculator.rb
@@ -11,18 +11,44 @@ def prompt(key, *args)
   puts("=> #{message}")
 end
 
-prompt('welcome')
-prompt('enter_amount')
-loan_amount = gets.chomp.to_i
-prompt('enter_apr')
-apr = gets.chomp.to_f
-prompt('loan_duration')
-loan_duration = gets.chomp.to_i
+def set_loan
+  prompt('enter_amount')
+  loan_amount = gets.chomp
+  if loan_amount.empty? || loan_amount.to_f < 0
+    prompt('positive')
+  end
+  loan_amount.to_f
+end
 
-apr = apr.to_f / 100
-monthly_interest = apr / 12
-months = loan_duration * 12
+def set_apr
+  prompt('enter_apr')
+  gets.chomp.to_f
+end
 
-monthly_payment = loan_amount * (monthly_interest / (1 - (1 + monthly_interest) ** (-months)))
-monthly_payment = monthly_payment.round(2)
-prompt('payment', monthly_payment)
+def set_duration
+  prompt('loan_duration')
+  gets.chomp.to_i
+end
+
+def monthly(apr)
+  apr = apr.to_f / 100
+  apr / 12
+end
+
+def monthly_payment(loan_amount, monthly_interest, months)
+  monthly_payment = loan_amount * (monthly_interest / (1 - ((1 + monthly_interest)**(-months))))
+  monthly_payment = monthly_payment.to_f.round(2)
+  prompt('payment', monthly_payment)
+end
+
+loop do
+  prompt('welcome')
+
+  loan_amount = set_loan
+  apr = set_apr
+  loan_duration = set_duration
+
+  monthly_interest = monthly(apr)
+  months = loan_duration * 12
+  monthly_payment(loan_amount, monthly_interest, months)
+end
