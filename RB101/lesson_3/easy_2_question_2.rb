5404a508e469578ca58642d87eedd166e0ba6759
MarilynRoxie <marilynroxie@gmail.com>
2024-02-03 08:55:39 -0800

    Added answers for easy 2, question 2 and started question 3

---
+++ RB101/lesson_3/easy_2_question_2.rb
@@ -0,0 +1,16 @@
+munsters_description = "The Munsters are creepy in a good way."
+
+# "tHE mUNSTERS ARE CREEPY IN A GOOD WAY." Note - I had never seen the swapcase method until now
+
+p munsters_description.swapcase!
+
+# "The munsters are creepy in a good way."
+p munsters_description.downcase.capitalize!
+
+# "the munsters are creepy in a good way."
+
+p munsters_description.downcase!
+
+# "THE MUNSTERS ARE CREEPY IN A GOOD WAY."
+
+p munsters_description.upcase!
