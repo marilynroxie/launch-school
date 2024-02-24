0d568c08b1ac1526304092d0dddb8142f23d54cc
MarilynRoxie <marilynroxie@gmail.com>
2024-02-23 16:43:56 -0800

    Added solution to Easy 3, problem 4

---
+++ R101-RB119-Small-Problems/4_easy_3/4_multiplyingtwonumbers.rb
@@ -0,0 +1,11 @@
+def multiply(num1, num2)
+  num1 * num2
+end
+
+p multiply(5, 3) == 15
+
+# Further Exploration:
+
+# If the first argument is an array, it adds the same set of numbers to the array num2 times.
+
+p multiply([1, 2, 3], 3)
