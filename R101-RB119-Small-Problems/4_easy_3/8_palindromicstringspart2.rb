90659ca38655682a181159a28d8a9dbbde50b301
MarilynRoxie <marilynroxie@gmail.com>
2024-03-01 18:12:36 -0600

    Added solution to easy 3, question 9

--- R101-RB119-Small-Problems/4_easy_3/8_palindromicstringspart2.rb
+++ R101-RB119-Small-Problems/4_easy_3/8_palindromicstringspart2.rb
@@ -6,10 +6,3 @@ def real_palindrome?(str)
   str = str.downcase.gsub(/\W/, "")
   palindrome?(str)
 end
-
-real_palindrome?("madam") == true
-real_palindrome?("Madam") == true           # (case does not matter)
-real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
-real_palindrome?("356653") == true
-p real_palindrome?("356a653")
-p real_palindrome?("123ab321")
---
+++ R101-RB119-Small-Problems/4_easy_3/9_palindromicnumbers.rb
@@ -0,0 +1,3 @@
+def palindromic_number?(num)
+  num == num.to_s.reverse.to_i
+end
