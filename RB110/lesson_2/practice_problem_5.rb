cceb6713a8827f13f1f6915ab829cfe723ef0e36
MarilynRoxie <marilynroxie@gmail.com>
2024-06-16 16:02:15 +0100

    Added solutions for RB110, L2, Practice 2-5

---
+++ RB110/lesson_2/practice_problem_5.rb
@@ -0,0 +1,14 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female" },
+}
+
+male_ages = 0
+munsters.each_value do |info|
+  male_ages += info["age"] if info["gender"] == "male"
+end
+
+male_ages
