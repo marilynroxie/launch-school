5420403964df1531bd8cca704802ac9555a326ec
MarilynRoxie <marilynroxie@gmail.com>
2024-06-23 11:49:42 +0100

    Added player turn walkthru code to TTT, L3

--- RB110/lesson_3/tic_tac_toe.rb
+++ RB110/lesson_3/tic_tac_toe.rb
@@ -1,3 +1,22 @@
+# 1. Display the initial empty 3x3 board.
+# 2. Ask the user to mark a square.
+# 3. Computer marks a square.
+# 4. Display the updated board state.
+# 5. If winner, display winner.
+# 6. If board is full, display tie.
+# 7. If neither winner nor board is full, go to #2
+# 8. Play again?
+# 9. If yes, go to #1
+# 10. Good bye!
+
+INITIAL_MARKER = " "
+PLAYER_MARKER = "X"
+COMPUTER_MARKER = "O"
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
 def display_board(brd)
   puts ""
   puts "    |    |"
@@ -16,9 +35,28 @@ end
 
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = " " }
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 board = initialize_board
 display_board(initialize_board)
+
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def player_places_piece!(brd)
+  square = ""
+  loop do
+    prompt "Choose a square(#{empty_squares(brd).join(", ")})"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that's not a valid choice."
+  end
+  brd[square] = PLAYER_MARKER
+end
+
+player_places_piece!(board)
+puts board.inspect
+display_board(board)
