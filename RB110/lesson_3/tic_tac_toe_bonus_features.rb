e19200086ecdc311a2eb2e8f53ce9980bf520aaf
MarilynRoxie <marilynroxie@gmail.com>
2024-06-27 09:24:06 +0100

    Expanded todo and set ground for incorporating RPS ideas into TTTbonus

--- RB110/lesson_3/tic_tac_toe_bonus_features.rb
+++ RB110/lesson_3/tic_tac_toe_bonus_features.rb
@@ -5,6 +5,11 @@
 # Computer AI: Offense
 # Computer turn refinements
 # Improve the game loop with place_piece and alternate_player method
+# Keep track of which square is which number
+# Extract messages to YAML?
+# Show scoreboard while playing
+# Allow entering names?
+# Implement ideas from RPS with bonus features
 
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
@@ -14,35 +19,44 @@ INITIAL_MARKER = " "
 PLAYER_MARKER = "X"
 COMPUTER_MARKER = "O"
 
-def prompt(msg)
-  puts "=> #{msg}"
+# ROUNDS_TO_WIN = 3
+
+# GRAND_WINNERS = {
+#   player: 0,
+#   computer: 0,
+#   player_streak: 0,
+#   computer_streak: 0
+# }
+
+def prompt(message)
+  puts "=> #{message}"
 end
 
 def display_markers
   puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
 end
 
-def display_line(brd)
+def display_line(board)
   3.times do |row|
-    display_row(brd, row)
+    display_row(board, row)
     puts "----------------" unless row == 2
   end
 end
 
-def display_row(brd, row)
+def display_row(board, row)
   puts "     |     |"
   row_start = row * 3
-  puts "  #{brd[row_start + 1]}  |  " \
-       "#{brd[row_start + 2]}  |  " \
-       "#{brd[row_start + 3]}"
+  puts "  #{board[row_start + 1]}  |  " \
+       "#{board[row_start + 2]}  |  " \
+       "#{board[row_start + 3]}"
   puts "     |     |"
 end
 
-def display_board(brd)
+def display_board(board)
   system "clear"
   display_markers
   puts ""
-  display_line(brd)
+  display_line(board)
   puts ""
 end
 
@@ -52,8 +66,8 @@ def initialize_board
   new_board
 end
 
-def empty_squares(brd)
-  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+def empty_squares(board)
+  board.keys.select { |num| board[num] == INITIAL_MARKER }
 end
 
 def joinor(arr, delimiter = ", ", word = "or")
@@ -66,39 +80,39 @@ def joinor(arr, delimiter = ", ", word = "or")
   end
 end
 
-def player_places_piece!(brd)
+def player_places_piece!(board)
   square = ""
   loop do
-    prompt "Choose a square to place a piece: #{joinor(empty_squares(brd))}"
+    prompt "Choose a square to place a piece: #{joinor(empty_squares(board))}"
     square = gets.chomp.to_i
-    break if empty_squares(brd).include?(square)
+    break if empty_squares(board).include?(square)
     prompt "Sorry, that's not a valid choice."
   end
-  brd[square] = PLAYER_MARKER
+  board[square] = PLAYER_MARKER
 end
 
-def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
-  brd[square] = COMPUTER_MARKER
+def computer_places_piece!(board)
+  square = empty_squares(board).sample
+  board[square] = COMPUTER_MARKER
 end
 
-def board_full?(brd)
-  empty_squares(brd).empty?
+def board_full?(board)
+  empty_squares(board).empty?
 end
 
-def detect_winner(brd)
+def detect_winner(board)
   WINNING_LINES.each do |line|
-    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+    if board.values_at(*line).count(PLAYER_MARKER) == 3
       return "Player"
-    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+    elsif board.values_at(*line).count(COMPUTER_MARKER) == 3
       return "Computer"
     end
   end
   nil
 end
 
-def someone_won?(brd)
-  !!detect_winner(brd)
+def someone_won?(board)
+  !!detect_winner(board)
 end
 
 loop do
@@ -126,5 +140,79 @@ loop do
   answer = gets.chomp
   break unless answer.downcase.start_with?("y")
 end
+
+# def match(score)
+#   until score[:player] == ROUNDS_TO_WIN || score[:computer] == ROUNDS_TO_WIN
+#     choice = set_choice
+#     computer_choice = computer_move
+#     display_choices(choice, computer_choice)
+#     update_score(choice, computer_choice, score)
+#     win_move_message(choice, computer_choice)
+#     display_scoreboard(score)
+#     display_results(choice, computer_choice)
+#   end
+# end
+
+# def grand_update(score)
+#   if score[:player] == ROUNDS_TO_WIN
+#     GRAND_WINNERS[:player] += 1
+#     GRAND_WINNERS[:player_streak] += 1
+#     GRAND_WINNERS[:computer_streak] = 0
+#   else
+#     GRAND_WINNERS[:computer] += 1
+#     GRAND_WINNERS[:computer_streak] += 1
+#     GRAND_WINNERS[:player_streak] = 0
+#   end
+# end
+
+# def grand_display(score)
+#   sleep 0.4
+#   if score[:player] == ROUNDS_TO_WIN
+#     puts messages('grand_winner')['player']
+#   else
+#     puts messages('grand_winner')['computer']
+#   end
+#   starred_message('separator')
+#   puts messages('total_grand_winners', GRAND_WINNERS[:player],
+#                 GRAND_WINNERS[:computer]).center(44)
+#   starred_message('separator')
+# end
+
+# def streak_display
+#   if GRAND_WINNERS[:player_streak] >= 2
+#     puts(messages('streak')['player'] % GRAND_WINNERS[:player_streak])
+#   elsif GRAND_WINNERS[:computer_streak] >= 2
+#     puts(messages('streak')['computer'] % GRAND_WINNERS[:computer_streak])
+#   end
+# end
+
+# def play_again(name)
+#   loop do
+#     prompt('play_again')
+#     answer = gets.chomp.strip.downcase
+#     if messages('options_pos').include?(answer)
+#       system 'clear'
+#       break
+#     elsif messages('options_neg').include?(answer)
+#       starred_message('thank_you', name)
+#       exit
+#     else
+#       system 'clear'
+#       prompt('invalid_choice')
+#     end
+#   end
+# end
+
+# name = get_name
+# starred_message('welcome', name)
+
+# loop do
+#   score = { player: 0, computer: 0 }
+#   match(score)
+#   grand_update(score)
+#   grand_display(score)
+#   streak_display
+#   play_again(name)
+# end
 
 prompt "Thanks for playing Tic Tac Toe! Good bye!"
