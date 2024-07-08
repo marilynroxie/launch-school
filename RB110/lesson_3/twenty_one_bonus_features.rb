ba93e7ac7bde1dca9269ba331e8398bbca94d989
MarilynRoxie <marilynroxie@gmail.com>
2024-07-08 08:27:00 +0100

    Changed suits to their unicode representation, 21 Bonus

--- RB110/lesson_3/twenty_one_bonus_features.rb
+++ RB110/lesson_3/twenty_one_bonus_features.rb
@@ -10,7 +10,7 @@ require "yaml"
 
 MESSAGES = YAML.load_file("twenty_one_messages.yml")
 
-SUITS = ["♠", "♥", "♦", "♣"]
+SUITS = ["\u2665", "\u2660", "\u2663", "\u2666"]
 
 VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
