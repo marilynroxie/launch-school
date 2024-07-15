24ca55badffe86df65f56d818e6f3aedb9ea5d7c
MarilynRoxie <marilynroxie@gmail.com>
2024-07-15 13:11:55 +0100

    Have suits instead of oblique unicode ref, 21 Bonus

--- RB110/lesson_3/twenty_one_bonus_features.rb
+++ RB110/lesson_3/twenty_one_bonus_features.rb
@@ -2,7 +2,7 @@ require "yaml"
 
 MESSAGES = YAML.load_file("twenty_one_messages.yml")
 
-SUITS = ["\u2665", "\u2660", "\u2666", "\u2663"]
+SUITS = ["♥", "♠", "♦", "♣"]
 
 VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
