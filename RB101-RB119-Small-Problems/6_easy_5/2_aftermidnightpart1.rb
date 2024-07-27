992e0ae1fd49a42496a67dee084c8c43d8602599
MarilynRoxie <marilynroxie@gmail.com>
2024-07-27 10:31:54 +0100

    Added solution to Easy 5, Exercise 3

--- RB101-RB119-Small-Problems/6_easy_5/2_aftermidnightpart1.rb
+++ RB101-RB119-Small-Problems/6_easy_5/2_aftermidnightpart1.rb
@@ -11,3 +11,13 @@ p time_of_day(-1437) == "00:03"
 p time_of_day(3000) == "02:00"
 p time_of_day(800) == "13:20"
 p time_of_day(-4231) == "01:29"
+
+# Further Exploration 1
+# Already satisfied in original solution
+
+# Further Exploration 2
+# How would you approach this problem if you were allowed to use ruby's Date and Time classes?
+
+# Further Exploration 3
+# How would you approach this problem if you were allowed to use ruby's Date and Time classes and wanted to consider the day of week in your calculation?
+# (Assume that delta_minutes is the number of minutes before or after midnight between Saturday and Sunday; in such a method, a delta_minutes value of -4231 would need to produce a return value of Thursday 01:29.)
---
+++ RB101-RB119-Small-Problems/6_easy_5/3_aftermidnightpart2.rb
@@ -0,0 +1,21 @@
+MINUTES_PER_HOUR = 60
+MINUTES_PER_DAY = 1440
+
+def after_midnight(time)
+  hours, minutes = time.split(":").map { |x| x.to_i }
+  (hours * MINUTES_PER_HOUR + minutes) % MINUTES_PER_DAY
+end
+
+def before_midnight(time)
+  (MINUTES_PER_DAY - after_midnight(time)) % MINUTES_PER_DAY
+end
+
+p after_midnight('00:00') == 0
+p before_midnight('00:00') == 0
+p after_midnight('12:34') == 754
+p before_midnight('12:34') == 686
+p after_midnight('24:00') == 0
+p before_midnight('24:00') == 0
+
+# Further Exploration
+# How would these methods change if you were allowed to use the Date and Time classes?
