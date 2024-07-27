805a5105d7f5c8600cb61d9856cb8a91e094d674
MarilynRoxie <marilynroxie@gmail.com>
2024-07-27 10:13:23 +0100

    Added solution to Easy 5, Exercise 2

---
+++ RB101-RB119-Small-Problems/6_easy_5/2_aftermidnightpart1.rb
@@ -0,0 +1,13 @@
+def time_of_day(number)
+  number = number % 1440
+  hours, minutes = number.divmod(60)
+  format('%02d:%02d', hours, minutes)
+end
+
+p time_of_day(0) == "00:00"
+p time_of_day(-3) == "23:57"
+p time_of_day(35) == "00:35"
+p time_of_day(-1437) == "00:03"
+p time_of_day(3000) == "02:00"
+p time_of_day(800) == "13:20"
+p time_of_day(-4231) == "01:29"
