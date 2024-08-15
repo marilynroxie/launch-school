478f1054661ba12c2eb8119b5e62cec6c4b24bcb
MarilynRoxie <marilynroxie@gmail.com>
2024-08-15 12:47:24 +0100

    Added solution for Easy 6, exercise 4

---
+++ RB101-RB119-Small-Problems/7_easy_6/4_reversedarrayspart1.rb
@@ -0,0 +1,51 @@
+def reverse!(arr)
+  left = 0
+  right = arr.length - 1
+
+  until left >= right
+    arr[left], arr[right] = arr[right], arr[left]
+    left += 1
+    right -= 1
+  end
+
+  arr
+end
+
+list = [1, 2, 3, 4]
+result = reverse!(list)
+p list
+p result == [4, 3, 2, 1] # true
+p list == [4, 3, 2, 1] # true
+p list.object_id == result.object_id # true
+
+list = [1, 2, 3, 4]
+result = reverse!(list)
+p list
+p result == [4, 3, 2, 1] # true
+p list == [4, 3, 2, 1] # true
+p list.object_id == result.object_id # true
+
+list = [1, 2, 3, 4]
+result = reverse!(list)
+p list
+p result == [4, 3, 2, 1] # true
+p list == [4, 3, 2, 1] # true
+p list.object_id == result.object_id # true
+
+list = [1, 2, 3, 4, 1]
+result = reverse!(list)
+p result == [1, 4, 3, 2, 1] # true
+p list == [1, 4, 3, 2, 1] # true
+p list.object_id == result.object_id # true
+
+list = %w(a b e d c)
+p reverse!(list) == ["c", "d", "e", "b", "a"] # true
+p list == ["c", "d", "e", "b", "a"] # true
+
+list = ['abc']
+p reverse!(list) == ["abc"] # true
+p list == ["abc"] # true
+
+list = []
+p reverse!(list) == [] # true
+p list == [] # true
