acd7cb3431ca3699e14cc97df7bc6296e61390d9
MarilynRoxie <marilynroxie@gmail.com>
2024-09-02 09:12:50 +0100

    Added solution for Easy 8, exercise 5

---
+++ RB101-RB119-Small-Problems/9_easy_8/5_fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(number, divisor)
+  loop do
+    if number % 3 == 0 && number % 5 == 0
+      puts "FizzBuzz"
+    elsif number % 3 == 0
+      puts "Fizz"
+    elsif number % 5 == 0
+      puts "Buzz"
+    else
+      puts number
+    end
+    number += 1
+    break if number > divisor
+  end
+end
+
+p fizzbuzz(1, 15) # -> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
