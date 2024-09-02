50de14f44071eca2079f2c324df718f71cce5e50
MarilynRoxie <marilynroxie@gmail.com>
2024-09-02 09:01:43 +0100

    Added solution to Easy 8, exercise 4

---
+++ RB101-RB119-Small-Problems/9_easy_8/4_palindromicsubstrings.rb
@@ -0,0 +1,34 @@
+def leading_substrings(str)
+  str.chars.inject([]) do |substr, char|
+    substr << (substr.last.to_s + char)
+  end
+end
+
+def substrings(str)
+  (0...str.length).map { |i| leading_substrings(str[i..-1]) }.flatten
+end
+
+p substrings('abcde') == [
+  'a', 'ab', 'abc', 'abcd', 'abcde',
+  'b', 'bc', 'bcd', 'bcde',
+  'c', 'cd', 'cde',
+  'd', 'de',
+  'e'
+]
+
+def palindromes(str)
+  substrings(str).select do |substring|
+    substring.size > 1 && substring == substring.reverse
+  end
+end
+
+p palindromes('abcd') == []
+p palindromes('madam') == ['madam', 'ada']
+p palindromes('hello-madam-did-madam-goodbye') == [
+  'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
+  'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
+  '-madam-', 'madam', 'ada', 'oo'
+]
+p palindromes('knitting cassettes') == [
+  'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
+]
