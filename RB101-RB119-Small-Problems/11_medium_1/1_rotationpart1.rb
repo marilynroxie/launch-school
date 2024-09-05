e0a2295f7c19c8a729e45393d6143f9a71be0cbc
MarilynRoxie <marilynroxie@gmail.com>
2024-09-05 10:11:21 +0100

    Modified solution for Medium 1, exercise 1

--- RB101-RB119-Small-Problems/11_medium_1/1_rotationpart1.rb
+++ RB101-RB119-Small-Problems/11_medium_1/1_rotationpart1.rb
@@ -1,6 +1,6 @@
 def rotate_array(arr)
-  num = arr.shift
-  arr << num
+  newarr = arr.dup
+  newarr << newarr.shift
 end
 
 p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
@@ -9,4 +9,4 @@ p rotate_array(["a"]) == ["a"]
 
 x = [1, 2, 3, 4]
 p rotate_array(x) == [2, 3, 4, 1]   # => true
-x == [1, 2, 3, 4]                 # => true
+p x == [1, 2, 3, 4]                 # => true
