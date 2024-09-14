376fb68d72d4411bab1e690228353332a483836d
MarilynRoxie <marilynroxie@gmail.com>
2024-09-14 15:00:01 +0100

    Added solution for Medium 1, exercise 7

---
+++ RB101-RB119-Small-Problems/11_medium_1/7_wordtodigit.rb
@@ -0,0 +1,22 @@
+def word_to_digit(str)
+  hash = {
+    "zero" => "0",
+    "one" => "1",
+    "two" => "2",
+    "three" => "3",
+    "four" => "4",
+    "five" => "5",
+    "six" => "6",
+    "seven" => "7",
+    "eight" => "8",
+    "nine" => "9"
+  }
+
+  hash.each do |key, value|
+    str.gsub!(/\b#{key}\b/, hash[key])
+  end
+
+  str
+end
+
+p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
