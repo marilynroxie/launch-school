b9d75f0c01b045d846ebfadd8b68dc3dc26e5138
MarilynRoxie <marilynroxie@gmail.com>
2024-09-21 09:56:28 +0100

    Added further exploration for Medium 2, exercise 3

--- RB101-RB119-Small-Problems/12_medium_2/3_lettercasepercentageratio.rb
+++ RB101-RB119-Small-Problems/12_medium_2/3_lettercasepercentageratio.rb
@@ -1,11 +1,14 @@
 def letter_percentages(str)
   {
-    lowercase: (str.count("a-z").to_f / str.length * 100),
-    uppercase: (str.count("A-Z").to_f / str.length * 100),
-    neither: ((str.length - str.count("a-z") - str.count("A-Z")).to_f / str.length * 100),
+    lowercase: (str.count("a-z").to_f / str.length * 100).round(1),
+    uppercase: (str.count("A-Z").to_f / str.length * 100).round(1),
+    neither: ((str.length - str.count("a-z") - str.count("A-Z")).to_f / str.length * 100).round(1),
   }
 end
 
 p letter_percentages("abCdef 123") == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
 p letter_percentages("AbCd +Ef") == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
 p letter_percentages("123") == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
+p letter_percentages("abcdefGHI") == { :lowercase => 66.7, :uppercase => 33.3, :neither => 0.0 }
+
+# Further exploration is incorporated for last example to round
