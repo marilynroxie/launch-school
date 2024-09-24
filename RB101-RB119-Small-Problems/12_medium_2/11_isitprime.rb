135a79daa1b4be4722639aeb82edf7fc746e9e6b
MarilynRoxie <marilynroxie@gmail.com>
2024-09-24 10:22:52 +0100

    Added solution to Medium 2, exercise 12

---
+++ RB101-RB119-Small-Problems/12_medium_2/11_isitprime.rb
@@ -0,0 +1,30 @@
+def is_prime(number)
+  case
+  when number == 2
+    true
+  when number <= 1 || number.even?
+    false
+  else
+    (3..Math.sqrt(number).to_i).step(2) do |i|
+      return false if number % i == 0
+    end
+    true
+  end
+end
+
+puts(is_prime(1) == false)              # true
+puts(is_prime(2) == true)               # true
+puts(is_prime(3) == true)               # true
+puts(is_prime(4) == false)              # true
+puts(is_prime(5) == true)               # true
+puts(is_prime(6) == false)              # true
+puts(is_prime(7) == true)               # true
+puts(is_prime(8) == false)              # true
+puts(is_prime(9) == false)              # true
+puts(is_prime(10) == false)             # true
+puts(is_prime(23) == true)              # true
+puts(is_prime(24) == false)             # true
+puts(is_prime(997) == true)             # true
+puts(is_prime(998) == false)            # true
+puts(is_prime(3_297_061) == true)       # true
+puts(is_prime(23_297_061) == false)     # true
