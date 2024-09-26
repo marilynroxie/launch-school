e85e2f0ee46da49346d2f5e423a84949d7623bce
MarilynRoxie <marilynroxie@gmail.com>
2024-09-26 10:32:17 +0100

    Added further exploration to Easy 6, exercise 3

--- RB101-RB119-Small-Problems/6_easy_5/3_aftermidnightpart2.rb
+++ RB101-RB119-Small-Problems/6_easy_5/3_aftermidnightpart2.rb
@@ -1,3 +1,5 @@
+require "time"
+
 MINUTES_PER_HOUR = 60
 MINUTES_PER_DAY = 1440
 
@@ -10,12 +12,28 @@ def before_midnight(time)
   -after_midnight(time) % MINUTES_PER_DAY
 end
 
-p after_midnight('00:00') == 0
-p before_midnight('00:00') == 0
-p after_midnight('12:34') == 754
-p before_midnight('12:34') == 686
-p after_midnight('24:00') == 0
-p before_midnight('24:00') == 0
+p after_midnight("00:00") == 0
+p before_midnight("00:00") == 0
+p after_midnight("12:34") == 754
+p before_midnight("12:34") == 686
+p after_midnight("24:00") == 0
+p before_midnight("24:00") == 0
 
 # Further Exploration
 # How would these methods change if you were allowed to use the Date and Time classes?
+
+def after_midnight(time)
+  time = Time.parse(time)
+  (time.hour * MINUTES_PER_HOUR + time.min) % MINUTES_PER_DAY
+end
+
+def before_midnight(time)
+  (MINUTES_PER_DAY - after_midnight(time)) % MINUTES_PER_DAY
+end
+
+p after_midnight("00:00") == 0
+p before_midnight("00:00") == 0
+p after_midnight("12:34") == 754
+p before_midnight("12:34") == 686
+p after_midnight("24:00") == 0
+p before_midnight("24:00") == 0
