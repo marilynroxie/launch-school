772b8d88450f4f472dbc2837ef7bfc86b50fbe04
MarilynRoxie <marilynroxie@gmail.com>
2025-05-31 19:54:55 +0100

    Added RB119 interview problems, problem 7 info

--- RB119/rb119_practice_problems_for_rb119_interview.rb
+++ RB119/rb119_practice_problems_for_rb119_interview.rb
@@ -323,3 +323,19 @@ p count_letters("W. E. B. Du Bois") == expected
 p count_letters("x") == { "x" => 1 }
 p count_letters("") == {}
 p count_letters("!!!") == {}
+
+# Problem 7
+
+# Create a method that takes an array of integers as an argument and returns the number of identical pairs of integers in that array. For instance, the number of identical pairs in [1, 2, 3, 2, 1] is 2: there are two occurrences each of both 2 and 1.
+
+# If the array is empty or contains exactly one value, return 0.
+
+# If a certain number occurs more than twice, count each complete pair once. For instance, for [1, 1, 1, 1] and [2, 2, 2, 2, 2], the method should return 2. The first array contains two complete pairs while the second has an extra 2 that isn't part of the other two pairs.
+
+p pairs([3, 1, 4, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7]) == 3
+p pairs([2, 7, 1, 8, 2, 8, 1, 8, 2, 8, 4]) == 4
+p pairs([]) == 0
+p pairs([23]) == 0
+p pairs([997, 997]) == 1
+p pairs([32, 32, 32]) == 1
+p pairs([7, 7, 7, 7, 7, 7, 7]) == 3
