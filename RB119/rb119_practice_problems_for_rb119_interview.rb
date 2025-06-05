051afb5bcf8f65881911d702343bd6d3a7ee168f
MarilynRoxie <marilynroxie@gmail.com>
2025-06-05 12:28:50 +0100

    Added solution to RB119 interview problem 17

--- RB119/rb119_practice_problems_for_rb119_interview.rb
+++ RB119/rb119_practice_problems_for_rb119_interview.rb
@@ -917,3 +917,55 @@ p distinct_multiples("7657") == 1              # 7
 p distinct_multiples("3141592653589793") == 4  # 3, 1, 5, 9
 
 p distinct_multiples("2718281828459045") == 5  # 2, 1, 8, 4, 5
+
+# Problem 17
+
+# Create a method that takes an array of integers as an argument. The method should determine the minimum integer value that can be appended to the array so the sum of all the elements equal the closest prime number that is greater than the current sum of the numbers. For example, the numbers in [1, 2, 3] sum to 6. The nearest prime number greater than 6 is 7. Thus, we can add 1 to the array to sum to 7.
+
+# Notes:
+
+# The array will always contain at least 2 integers.
+# All values in the array must be positive (> 0).
+# There may be multiple occurrences of the various numbers in the array.
+
+# Understanding the Problem
+
+# inputs: array of integers
+# outputs: integer (representing closest prime number greater than array.sum)
+# explicit: array will always contain >= 2 integers, all integer elements will be > 0 (positive), same number may occur more than once
+# implicit: argument will not be an empty array, sum may or may not already be prime
+
+# Data structure
+# Array when working with sums
+
+# Algorithm
+# Require prime module
+# Get sum of array argument
+# Loop through sum += 1
+# Break as soon as the sum is prime
+# Return sum - arr.sum
+
+# Code
+
+def nearest_prime_sum(arr)
+  require "prime"
+
+  sum = arr.sum
+
+  loop do
+    sum += 1
+    break if sum.prime?
+  end
+
+  sum - arr.sum
+end
+
+# Examples / Test Cases
+
+p nearest_prime_sum([1, 2, 3]) == 1        # Nearest prime to 6 is 7
+p nearest_prime_sum([5, 2]) == 4           # Nearest prime to 7 is 11
+p nearest_prime_sum([1, 1, 1]) == 2        # Nearest prime to 3 is 5
+p nearest_prime_sum([2, 12, 8, 4, 6]) == 5 # Nearest prime to 32 is 37
+
+# Nearest prime to 163 is 167
+p nearest_prime_sum([50, 39, 49, 6, 17, 2]) == 4
