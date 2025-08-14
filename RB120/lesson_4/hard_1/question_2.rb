d8991fe6e2cfe85af94d7b8105208335c46828de
MarilynRoxie <marilynroxie@gmail.com>
2025-08-14 11:52:10 -0500

    Added solutions ro RB120 Lesson 4, Hard 1

---
+++ RB120/lesson_4/hard_1/question_2.rb
@@ -0,0 +1,53 @@
+module Fuelable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_efficiency, :fuel_capacity
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class WheeledVehicle
+  include Fuelable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Auto < WheeledVehicle
+  def initialize
+    super([30, 30, 32, 32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    super([20, 20], 80, 8.0)
+  end
+end
+
+class Seafarer
+  include Fuelable
+
+  attr_reader :propeller_count, :hull_count
+end
+
+class Catamaran < Seafarer
+end
+
+class Motorboat < Seafarer
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
