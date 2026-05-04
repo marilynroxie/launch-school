f8598a3b2498a7052ebdd4e94d94d035d9a2a1f5
MarilynRoxie <marilynroxie@gmail.com>
2026-05-03 20:55:45 -0700

    Added drafts of exercise 3 & 4, finished exercise 6

---
+++ RB130/RB130-Ruby-Foundations-More-Topics/easy_1/exercise_6.rb
@@ -0,0 +1,19 @@
+def any?(arr)
+  count = 0
+  until count == arr.size
+    if yield(arr[count])
+      return true
+    else
+      count += 1
+    end
+  end
+  false
+end
+
+p any?([1, 3, 5, 6]) { |value| value.even? } == true
+p any?([1, 3, 5, 7]) { |value| value.even? } == false
+p any?([2, 4, 6, 8]) { |value| value.odd? } == false
+p any?([1, 3, 5, 7]) { |value| value % 5 == 0 } == true
+p any?([1, 3, 5, 7]) { |value| true } == true
+p any?([1, 3, 5, 7]) { |value| false } == false
+p any?([]) { |value| true } == false
