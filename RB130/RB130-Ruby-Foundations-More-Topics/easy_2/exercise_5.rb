a1a1c3ac1e80f02c67e43752579ac4964a0516aa
MarilynRoxie <marilynroxie@gmail.com>
2026-05-06 14:24:16 -0700

    Added solution for exercise 6

---
+++ RB130/RB130-Ruby-Foundations-More-Topics/easy_2/exercise_5.rb
@@ -0,0 +1,16 @@
+def drop_while(*args)
+  arr = []
+  args.flatten.each_with_index do |element, idx|
+    if yield(element) == false
+      arr << element
+    end
+  end
+  arr
+end
+
+p drop_while([1, 3, 5, 6]) { |value| value.odd? } == [6]
+p drop_while([1, 3, 5, 6]) { |value| value.even? } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| true } == []
+p drop_while([1, 3, 5, 6]) { |value| false } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| value < 5 } == [5, 6]
+p drop_while([]) { |value| true } == []
